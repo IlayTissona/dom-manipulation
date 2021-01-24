c39b0c79d7f0827a98687c1dafecd6dbd7e7207d
IlayTissona <ilay211@gmail.com>
2021-01-24 12:34:54 +0200

    task3

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let input = document.querySelector("#item");
 let list = document.querySelector("ul");
 let button = document.querySelector("button");
-button.onclick(run());
+button.onclick = run;
+function run() {
+	let value = input.value;
+}
